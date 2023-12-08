d0bfd1d8ec58a985f13e9797e2829fcb773a076e
Uxinnn <xinyu.foong@gmail.com>
2023-12-08 16:40:48 +0800

    Added script to create files needed for each day.

---
+++ generate_day_dir.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+
+YEAR = "2023"
+DAY = "09"
+
+
+# Create directory
+dir_path = Path(f"./{YEAR}/day{DAY}")
+try:
+    dir_path.mkdir(parents=True)
+    print(f"Directory {dir_path} created.")
+except FileExistsError as e:
+    print(f"Directory {dir_path} already exists.")
+
+
+# Create python file
+py_file_path = dir_path / f"day{DAY}.py"
+if py_file_path.is_file():
+    print(f"Python file {py_file_path} already exists.")
+else:
+    with open(py_file_path, 'w') as f:
+        pass
+    print(f"Python file {py_file_path} created.")
+
+
+# Create example file
+eg_file_path = dir_path / "example.in"
+if eg_file_path.is_file():
+    print(f"Example file {eg_file_path} already exists.")
+else:
+    with open(eg_file_path, 'w') as f:
+        pass
+    print(f"Example file {eg_file_path} created.")
+
+
+# Create input file
+in_file_path = dir_path / "input.in"
+if in_file_path.is_file():
+    print(f"Input file {in_file_path} already exists.")
+else:
+    with open(in_file_path, 'w') as f:
+        pass
+    print(f"Input file {in_file_path} created.")
+
+# Create question file
+qn_file_path = dir_path / "question.txt"
+if qn_file_path.is_file():
+    print(f"Question file {qn_file_path} already exists.")
+else:
+    with open(qn_file_path, 'w') as f:
+        pass
+    print(f"Question file {qn_file_path} created.")
